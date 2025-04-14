d868275e8474af3d6b6215985546dcb083f931d5
karnelll <165611407+karnelll@users.noreply.github.com>
2025-04-14 14:27:35 +0900

    [fix] 빌드 오류 수정

--- fe/src/app/layout.tsx
+++ fe/src/app/layout.tsx
@@ -1,18 +1,22 @@
-import "./globals.css"
-import type { Metadata } from "next"
+import "./globals.css";
+import type { Metadata } from "next";
 import LayoutWrapper from "@/components/common/LayoutWrapper";
 
 export const metadata: Metadata = {
   title: "어르심",
   description: "노인을 위한 활동 추천 플랫폼",
-}
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="ko">
       <body className="font-sans text-[18px] leading-relaxed bg-white">
         <LayoutWrapper>{children}</LayoutWrapper>
       </body>
     </html>
-  )
+  );
 }
