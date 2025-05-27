1ef01d8f6f93d1563462a6e577e439aeb92359d8
subeend <soorud05@kyonggi.ac.kr>
2025-05-27 15:59:01 +0900

    [fix]빌드오류

---
+++ fe/src/components/chatbot/ChatbotHeader.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import type { NextPage } from "next";
+import { useRouter } from "next/navigation";
+import BackButton from "@/components/common/button/BackButton";
+
+interface ChatbotHeaderProps {
+  onBack?: () => void;
+}
+
+const ChatbotHeader: NextPage<ChatbotHeaderProps> = ({ onBack }) => {
+  const router = useRouter();
+
+  const handleBack = (
+    e: React.MouseEvent<HTMLAnchorElement | HTMLButtonElement>,
+  ) => {
+    e.preventDefault();
+    if (onBack) {
+      onBack();
+    } else {
+      router.back();
+    }
+  };
+
+  return (
+    <div className="w-full relative bg-white h-14 md:h-16 flex items-center px-2 md:px-4 text-gray font-pretendard">
+      <BackButton onClick={handleBack} className="mr-1" />
+
+      <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 font-medium text-[21px] md:text-2xl">
+        챗봇
+      </div>
+    </div>
+  );
+};
+
+export default ChatbotHeader;
